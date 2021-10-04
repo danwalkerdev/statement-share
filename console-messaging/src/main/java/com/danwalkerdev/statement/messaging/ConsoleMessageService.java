ed1030010e3c1a21c9d7b920cb4090df1a7a1713
dan.walker <dan@danwalker.dev>
2021-10-04 02:26:43 +0100

    Add console message provider

--- console-messaging/src/main/java/com/danwalkerdev/statement/messaging/ConsoleMessageService.java
+++ console-messaging/src/main/java/com/danwalkerdev/statement/messaging/ConsoleMessageService.java
@@ -3,7 +3,6 @@ package com.danwalkerdev.statement.messaging;
 import com.danwalkerdev.statement.api.Transaction;
 
 import java.util.Collection;
-import java.util.stream.Stream;
 
 public class ConsoleMessageService implements MessageService {
 
